685f41163f8407a235287aec0a8153b507c48482
Trevor Peterson <tpeterson@2u.com>
2021-11-18 14:11:01 -0600

    Adds solution project generated via start.spring.io

---
+++ Solution/Ledger-API/src/main/java/com/twou/LedgerAPI/LedgerApiApplication.java
@@ -0,0 +1,13 @@
+package com.twou.LedgerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedgerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LedgerApiApplication.class, args);
+	}
+
+}
---
+++ Solution/Ledger-API/src/test/java/com/twou/LedgerAPI/LedgerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twou.LedgerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
